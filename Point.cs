03b821a7148e6466265c38c671f9f874b2dac007
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2024-10-08 07:56:56 +0300

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Point
+    {
+        private double x;
+        private double y;
+
+        public Point (double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double DistanceBetweenTwoPoint (Point other)
+        {
+
+            return Math.Sqrt(Math.Pow(this.x - other.x, 2) + Math.Pow(this.y - other.y, 2));
+        }}
+    }
+}
